494dea5f6871771568b7515e3f872895d899c199
Ivan Milincic <lokomotivan@gmail.com>
2017-03-02 13:50:40 +0100

    added gulp less

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Reload browser after file save with gulp + browsersync. Resurce https://www.brow
 2. npm install gulp -D
 3. npm install browser-sync gulp --save-dev
 4. gulp (run gulp)
+// less
+1. npm install gulp-less
+2. npm install gulp-cssmin
+3. npm install gulp-plumber
---
+++ gulp-less-browsersync/gulpfile.js
@@ -0,0 +1,40 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+// less
+less = require('gulp-less'),
+cssmin = require('gulp-cssmin'),
+plumber = require('gulp-plumber'),
+rename = require('gulp-rename');
+
+// Relaod
+gulp.task('reload', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
+    gulp.watch("./less/*.less").on("change", reload);
+});
+
+// Less
+gulp.task('watch', function () {
+    gulp.watch('./less/*.less', ['less']);
+});
+gulp.task('less', function () {
+    gulp.src('./less/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('./css/'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'))
+
+});
+
+gulp.task('default', ['less', 'watch', 'reload']);
