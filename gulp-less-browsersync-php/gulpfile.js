a918a21735577ba6a27bb4d484b7e86d05363da6
Ivan Milincic <lokomotivan@gmail.com>
2017-03-18 13:38:12 +0100

    added error handling

--- gulp-less-browsersync-php/gulpfile.js
+++ gulp-less-browsersync-php/gulpfile.js
@@ -5,6 +5,7 @@ var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 var connect     = require('gulp-connect-php');
 // LESS related modules
+plumber         = require('gulp-plumber');
 var less        = require('gulp-less');
 var autoprefix  = require('gulp-autoprefixer');
 var cssmin      = require('gulp-cssmin');
@@ -14,25 +15,41 @@ var rename      = require('gulp-rename');
 // Compile LESS, autoprefix CSS3, minify...
 // and save to target CSS directory
 gulp.task('less', function () {
+
+    var onError = function(err) {
+      notify.onError({
+          title:    "Error compiling less file",
+          subtitle: "<%= error.filename.replace(/^.*[\\\/]/, '') %>",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+        })(err);
+      this.emit('end')
+    };
+
     return gulp.src('./less/theme.less')
+        .pipe(plumber({errorHandler: onError}))
         .pipe(less())
         .pipe(autoprefix('last 2 version'))
+        .pipe(notify("Succesfully compiled <%= file.relative %>!"))
         .pipe(gulp.dest('./css'))
         .pipe(cssmin())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('./css'))
-        .pipe(notify('LESS Compiled'))
+        //.pipe(notify('LESS Compiled'))
+
 });
 // Keep an eye on less files for changes...
 gulp.task('watch', function () {
-    gulp.watch('./less/custom/*.less', ['less']);
-    gulp.watch('./less/*.less', ['less']);
+    gulp.watch('./less/_custom/*.less', ['less']);
+    gulp.watch('./less/theme.less', ['less']);
     // reload page whe dose fiels change
     gulp.watch("*.php").on("change", reload);
+    gulp.watch("./inc/*.php").on("change", reload);
     gulp.watch("./js/*.js").on("change", reload);
     gulp.watch("./css/*.css").on("change", reload);
+    gulp.watch("./less/_custom/*.less").on("change", reload);
 });
 
 // Connect to Localhost & Browsersync
