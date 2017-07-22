fdebbeab9c76711d6ca8e1f14226bb5feeec64c5
Ivan Milincic <lokomotivan@gmail.com>
2017-07-22 22:43:29 +0200

    gulp tastk rewrite

---
+++ gulp-localhost/gulpfile.js
@@ -0,0 +1,69 @@
+var gulp    = require('gulp');
+var notify  = require('gulp-notify');
+// RELOAD modules
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+var connect     = require('gulp-connect-php');
+// LESS related modules
+var plumber     = require('gulp-plumber');
+var less        = require('gulp-less');
+var autoprefix  = require('gulp-autoprefixer');
+var cssmin      = require('gulp-cssmin');
+var rename      = require('gulp-rename');
+
+
+// Compile LESS, autoprefix CSS3, minify...
+// and save to target CSS directory
+gulp.task('less', function () {
+
+    var onError = function(err) {
+      notify.onError({
+          title:    "Error compiling less file",
+          subtitle: "<%= error.filename.replace(/^.*[\\\/]/, '') %>",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+        })(err);
+      this.emit('end')
+    };
+
+    return gulp.src('less/theme.less')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(less())
+        .pipe(autoprefix('last 2 version'))
+        .pipe(notify("Succesfully compiled <%= file.relative %>!"))
+        .pipe(gulp.dest('css'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('css'))
+        //.pipe(notify('LESS Compiled'))
+
+});
+
+// Keep an eye on less files for changes...
+gulp.task('watch', function () {
+    // Main Files to watch
+    gulp.watch("*.html").on("change", reload);
+    gulp.watch("*.php").on("change", reload);
+    // less/css/js to watch
+    gulp.watch('./less/custom/*.less', ['less']);
+    gulp.watch('./less/vars.less', ['less']);
+    gulp.watch('./less/theme.less', ['less']);
+    gulp.watch('./less/responsive.less', ['less']);
+    gulp.watch("./css/*.css").on("change", reload);
+});
+
+// Connect to Localhost & Browsersync
+gulp.task('connect-sync', function() {
+  connect.server({}, function (){
+        browserSync.init({
+            proxy: '127.0.0.1:8000', // localhost root
+            baseDir: "./_Baumrock/graphic-design/", // define base dir here
+            browser: "firefox"
+        });
+    });
+});
+
+// What tasks does running gulp trigger?
+gulp.task('default', ['less', 'watch', 'connect-sync']);
---
+++ gulp-localhost/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-gb" dir="ltr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My Project</title>
+        <link rel="shortcut icon" href="docs/images/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
+
+        <!-- css -->
+        <link rel="stylesheet" href="css/theme.css">
+        <link rel="stylesheet" href="css/custom.css">
+
+        <!-- scripts -->
+        <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+        <script src="js/main.js"></script>
+
+    </head>
+
+    <body>
+        <h1>O Yeah</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </body>
+
+
+</html>
